773b30ce0adacc2e731850b97482420a1a46b53b
subochev <caffeine.mgn@gmail.com>
2019-05-23 14:53:47 +0300

    Added Tools For ThreadPool

---
+++ job/src/commonMain/kotlin/pw/binom/job/Executor.kt
@@ -0,0 +1,7 @@
+package pw.binom.job
+
+import pw.binom.io.Closeable
+
+interface Executor<T, R> : Closeable {
+    fun execute(value: T, promise: ResumableFuturePromise<R>?)
+}
---
+++ job/src/commonMain/kotlin/pw/binom/job/FixedThreadPool.kt
@@ -0,0 +1,70 @@
+package pw.binom.job
+
+import pw.binom.FreezedStack
+import pw.binom.Queue
+import pw.binom.atomic.AtomicBoolean
+import pw.binom.doFreeze
+import pw.binom.neverFreeze
+
+class FixedThreadPool<P, R>(size: Int, taskExecutor: () -> WorkerExecutor<P, R>) : Executor<P, R> {
+    init {
+        neverFreeze()
+    }
+
+    class Data<P, R>(val value: P, private val promise: ResumableFuturePromise<R>?) {
+
+        fun resolve(value: R) {
+            promise?.resume(value)
+        }
+
+        fun exception(exception: Throwable) {
+            promise?.exception(exception)
+        }
+    }
+
+    class Controller<P, R>(val queue: Queue<Data<P, R>>, val taskExecutor: () -> WorkerExecutor<P, R>) {
+
+        private val _interrupted = AtomicBoolean(false)
+
+
+        val isInterrupted: Boolean
+            get() = _interrupted.value
+
+        fun interrupt() {
+            if (isInterrupted)
+                throw IllegalStateException("Controller already interrupted")
+            _interrupted.value = true
+        }
+
+        init {
+            doFreeze()
+        }
+    }
+
+    interface WorkerExecutor<P, R> {
+        fun start(controller: Controller<P, R>)
+    }
+
+    private val queue = FreezedStack<Data<P, R>>().asFiFoQueue()
+
+    private val controller = Controller(queue, taskExecutor)
+
+    override fun execute(value: P, promise: ResumableFuturePromise<R>?) {
+        queue.push(Data(value, promise))
+    }
+
+    override fun close() {
+        controller.interrupt()
+        workers.forEach {
+            it.close()
+        }
+    }
+
+    private val workers = Array(size) {
+        val w = Worker()
+        w.execute({ controller }) {
+            it.taskExecutor().start(it)
+        }
+        w
+    }
+}
