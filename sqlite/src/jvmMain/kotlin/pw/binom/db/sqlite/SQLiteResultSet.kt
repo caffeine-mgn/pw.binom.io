3947782c1ce8448c6a264e9a40fa7a387718c6dc
anton.subochev <anton.subochev@alibaba-inc.com>
2020-10-17 13:49:33 +0300

    refactoring

--- sqlite/src/jvmMain/kotlin/pw/binom/db/sqlite/SQLiteResultSet.kt
+++ sqlite/src/jvmMain/kotlin/pw/binom/db/sqlite/SQLiteResultSet.kt
@@ -4,6 +4,7 @@ import pw.binom.db.SQLException
 import java.sql.ResultSet
 
 class SQLiteResultSet(private val native: ResultSet) : pw.binom.db.ResultSet {
+
     override val columns: List<String> by lazy {
         val count = native.metaData.columnCount
         val out = ArrayList<String>(count)
@@ -14,41 +15,37 @@ class SQLiteResultSet(private val native: ResultSet) : pw.binom.db.ResultSet {
     }
 
     override fun next() = native.next()
-    private fun nullNotExpected(): Nothing = throw SQLException("Column value is null")
-    override fun getString(index: Int): String = native.getString(index + 1)?:nullNotExpected()
-    override fun getString(column: String): String = native.getString(column)?:nullNotExpected()
+    override fun getString(index: Int): String? = native.getString(index + 1)
+    override fun getString(column: String): String? = native.getString(column)
 
-    override fun getBoolean(index: Int) = native.getBoolean(index + 1)?:nullNotExpected()
-    override fun getBoolean(column: String): Boolean = native.getBoolean(column)?:nullNotExpected()
+    override fun getBoolean(index: Int): Boolean? = getInt(index + 1)?.let { it > 0 }
+    override fun getBoolean(column: String): Boolean? = getInt(column)?.let { it > 0 }
 
-    override fun getInt(index: Int) = native.getInt(index + 1)?:nullNotExpected()
-    override fun getInt(column: String): Int = native.getInt(column)?:nullNotExpected()
+    override fun getInt(index: Int): Int? = native.getInt(index + 1)
+    override fun getInt(column: String): Int? = native.getInt(column)
 
-    override fun getLong(index: Int) = native.getLong(index + 1)?:nullNotExpected()
-    override fun getLong(column: String): Long = native.getLong(column)?:nullNotExpected()
+    override fun getLong(index: Int): Long? = native.getLong(index + 1)
+    override fun getLong(column: String): Long? = native.getLong(column)
 
-    override fun getFloat(index: Int) = native.getFloat(index + 1)?:nullNotExpected()
-    override fun getFloat(column: String): Float = native.getFloat(column)?:nullNotExpected()
-    override fun getBlob(index: Int): ByteArray {
-        return (native.getBinaryStream(index + 1)?:nullNotExpected()).readAllBytes()
-//        val b = native.getBlob(index + 1)
-//        val buf = ByteArray(b.length().toInt())
-//        b.getBytes(0, buf.size)
-//        return buf
+    override fun getFloat(index: Int): Float? = native.getFloat(index + 1)
+    override fun getFloat(column: String): Float? = native.getFloat(column)
+    override fun getBlob(index: Int): ByteArray? {
+        val stream = native.getBinaryStream(index + 1) ?: return null
+        return stream.readAllBytes()
     }
 
-    override fun getBlob(column: String): ByteArray {
-        val b = native.getBlob(column)?:nullNotExpected()
+    override fun getBlob(column: String): ByteArray? {
+        val b = native.getBlob(column) ?: return null
         val buf = ByteArray(b.length().toInt())
         b.getBytes(0, buf.size)
         return buf
     }
 
     override fun isNull(index: Int) =
-            native.getObject(index + 1) == null
+        native.getObject(index + 1) == null
 
     override fun isNull(column: String): Boolean =
-            native.getObject(column) == null
+        native.getObject(column) == null
 
     override fun close() {
         native.close()
