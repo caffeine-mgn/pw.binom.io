89cb85f2a732d525b61d4950ffaf10dd78e4b127
subochev <caffeine.mgn@gmail.com>
2020-12-21 01:21:32 +0300

    fix

--- env/src/commonMain/kotlin/pw/binom/Platform.kt
+++ env/src/commonMain/kotlin/pw/binom/Platform.kt
@@ -5,6 +5,8 @@ enum class Platform {
     MINGW_X86,
     LINUX_ARM_32,
     LINUX_ARM_64,
+    LINUX_MIPS_32,
+    LINUX_MIPSEL_32,
     LINUX_64,
     MACOS,
     JVM,
---
+++ env/src/linuxMips32Main/kotlin/pw/binom/Environment.kt
@@ -0,0 +1,49 @@
+package pw.binom
+
+import kotlinx.cinterop.*
+import platform.posix.*
+
+actual val Environment.isBigEndian: Boolean
+    get() = isBigEndianPrivate
+
+actual val Environment.platform: Platform
+    get() = Platform.LINUX_MIPS_32
+
+actual fun Environment.getEnvs(): Map<String, String> {
+    val out = HashMap<String, String>()
+    var i = 0
+    while (true) {
+        val line = (__environ!!.get(i++) ?: break).toKString()
+        val items = line.split('=', limit = 2)
+        out[items[0]] = items[1]
+    }
+    return out
+}
+
+actual val Environment.workDirectory: String
+    get() {
+        val data = getcwd(null, 0.convert()) ?: TODO()
+        if (errno == EACCES)
+            throw RuntimeException("Forbidden")
+        try {
+            return data.toKString()
+        } finally {
+            free(data)
+        }
+    }
+
+actual val Environment.currentTimeMillis: Long
+    get() = memScoped {
+        val ff = alloc<timespec>()
+        clock_gettime(CLOCK_REALTIME, ff.ptr)
+        ff.tv_sec * 1000L + ff.tv_nsec / 1000000L
+    }
+
+actual val Environment.currentTimeNanoseconds: Long
+    get() = memScoped {
+        val ff = alloc<timespec>()
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, ff.ptr)
+        ff.tv_sec * 1000000000L + ff.tv_nsec
+    }
+
+actual fun Environment.getEnv(name: String): String? = getenv(name)?.toKString()
