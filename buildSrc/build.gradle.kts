36ae74bd4e0e13f5a99138efdab8518fc58db6f3
caffeine-mgn <caffeine.mgn@gmail.com>
2021-01-29 04:14:50 +0300

    * Fix bug * Fix tests

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,30 @@
+buildscript {
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        jcenter()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+    }
+}
+
+plugins{
+        kotlin("jvm") version "1.4.21"
+        //id("kotlin-platform-jvm")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    api("org.jetbrains.kotlin:kotlin-stdlib:1.4.21")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+    api("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.4.21")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+}
