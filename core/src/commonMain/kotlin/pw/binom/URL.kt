41c6fbc71a38d1dfba013d8896c5a3a3f17f9873
caffeine-mgn <caffeine.mgn@gmail.com>
2021-03-05 18:29:58 +0300

    url fix

--- core/src/commonMain/kotlin/pw/binom/URL.kt
+++ core/src/commonMain/kotlin/pw/binom/URL.kt
@@ -1,6 +1,236 @@
 package pw.binom
 
-class URL(private val path: String) {
+inline class URL internal constructor(val path: String) {
+    companion object {
+        fun new(
+            protocol: String?,
+            user: String?,
+            password: String?,
+            host: String,
+            port: Int?,
+            uri: String,
+            query: String?,
+            hash: String?,
+        ): String {
+            val sb = StringBuilder()
+            if (protocol != null) {
+                sb.append(protocol).append(":")
+            }
+            sb.append("//")
+            if (user != null) {
+                sb.append(user)
+            }
+            val auth = user != null || password != null
+            if (password != null) {
+                sb.append(":")
+                sb.append(password)
+            }
+            if (user != null) {
+                sb.append("@")
+            }
+            sb.append(host)
+            if (port != null) {
+                sb.append(":").append(port)
+            }
+            sb.append(uri)
+            if (query != null) {
+                sb.append("?").append(query)
+            }
+            if (hash != null) {
+                sb.append("#").append(hash)
+            }
+            return sb.toString()
+        }
+    }
+
+    val protocol: String?
+        get() {
+            val p = path.indexOf("//")
+            if (p == 0) {
+                return null
+            }
+            return path.substring(0, p - 1)
+        }
+    val user: String?
+        get() {
+            val p1 = path.indexOf('@')
+            if (p1 == -1) {
+                return null
+            }
+            val p = path.indexOf("//") + 2
+            val b = path.indexOf(':', p + 2)
+            if (b == -1 || b > p1) {
+                return path.substring(p, p1)
+            }
+
+            return path.substring(p, b)
+        }
+    val password: String?
+        get() {
+            val p1 = path.indexOf('@')
+            if (p1 == -1) {
+                return null
+            }
+            val p = path.indexOf("//") + 2
+            val b = path.indexOf(':', p + 2)
+            if (b == -1 || b > p1) {
+                return null
+            }
+
+            return path.substring(b + 1, p1)
+        }
+    val host: String
+        get() {
+            var p = path.indexOf('@') + 1
+            if (p == 0) {
+                p = path.indexOf("//") + 2
+            }
+
+            var e = path.indexOf(':', p + 1)
+            if (e == -1) {
+                e = path.indexOf('/', p + 1)
+            }
+            if (e == -1) {
+                e = path.indexOf('?', p + 1)
+            }
+
+            if (e == -1) {
+                e = path.indexOf('#', p + 1)
+            }
+            if (e == -1) {
+                e = path.length
+            }
+            val t = path.substring(p, e)
+            return path.substring(p, e)
+        }
+    val port: Int?
+        get() {
+            var s = path.indexOf("//") + 2
+            s = path.indexOf(":", s)
+            val c = path.indexOf('@')
+            if (c != -1) {
+                s = path.indexOf(":", c + 1)
+                if (s == -1) {
+                    return null
+                }
+            }
+            if (s == -1) {
+                return null
+            }
+            s++
+
+            val hash = path.indexOf('#', s)
+            val query = path.indexOf('?', s)
+            val end = path.indexOf('/', s)
+            var e = path.length
+            e = when {
+                end != -1 && end < e -> end
+                query != -1 && query < e -> query
+                hash != -1 && hash < e -> hash
+                else -> e
+            }
+            return path.substring(s, e).toInt()
+        }
+    val uri: String
+        get() {
+            var s = path.indexOf("//") + 2
+            s = path.indexOf('/', s)
+            if (s == -1) {
+                return ""
+            }
+            var e = path.indexOf('?')
+            if (e == -1) {
+                e = path.indexOf('#')
+            }
+            if (e == -1) {
+                e = path.length
+            }
+            return path.substring(s, e)
+        }
+    val query: String?
+        get() {
+            val s = path.indexOf('?')
+            if (s == -1) {
+                return null
+            }
+            var e = path.indexOf('#')
+            if (e == -1) {
+                e = path.length
+            }
+            return path.substring(s, e)
+        }
+    val hash: String?
+        get() {
+            val s = path.indexOf('#')
+            if (s == -1) {
+                return null
+            }
+            return path.substring(s)
+        }
+
+    fun copy(
+        protocol: String? = this.protocol,
+        user: String? = this.user,
+        password: String? = this.password,
+        host: String = this.host,
+        port: Int? = this.port,
+        uri: String = this.uri,
+        query: String? = this.query,
+        hash: String? = this.hash,
+    ) =
+        URL(
+            new(
+                protocol = protocol,
+                user = user,
+                password = password,
+                host = host,
+                port = port,
+                uri = uri,
+                query = query,
+                hash = hash
+            )
+        )
+
+    override fun toString(): String = new(
+        protocol = protocol,
+        user = user,
+        password = password,
+        host = host,
+        port = port,
+        uri = uri,
+        query = query,
+        hash = hash
+    )
+}
+
+fun String.toURLOrNull(): URL? {
+    val p = indexOf("//")
+    if (p == -1) {
+        return null
+    }
+    if (indexOf("//", p + 1) != -1) {
+        return null
+    }
+    val q = indexOf("?")
+    if (q != -1 && q < p) {
+        return null
+    }
+    if (indexOf("?", q + 1) != -1) {
+        return null
+    }
+
+    val h = indexOf("#")
+    if (h != -1 && q > 0 && h < q) {
+        return null
+    }
+    if (indexOf("#", h + 1) != -1) {
+        return null
+    }
+
+    return URL(this)
+}
+
+class URL2(private val path: String) {
     val protocol: String?
     val host: String
     val port: Int?
--- core/src/commonTest/kotlin/pw/binom/TestURL.kt
+++ core/src/commonTest/kotlin/pw/binom/TestURL.kt
@@ -2,12 +2,127 @@ package pw.binom
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
 class TestURL {
 
     @Test
-    fun `proto addres port`() {
+    fun credentialTest() {
+        "https://user:password@binom/".toURLOrNull()!!.apply {
+            assertEquals("user", user)
+            assertEquals("password", password)
+        }
+        "https://user@binom/".toURLOrNull()!!.apply {
+            assertEquals("user", user)
+            assertNull(password)
+        }
+        "https://@binom/".toURLOrNull()!!.apply {
+            assertEquals("", user)
+            assertNull(password)
+        }
+        "https://user:@binom/".toURLOrNull()!!.apply {
+            assertEquals("user", user)
+            assertEquals("", password)
+        }
+    }
+
+    @Test
+    fun protoTest() {
+        "https://binom/".toURLOrNull()!!.apply {
+            assertEquals("https", protocol)
+        }
+        "//binom/".toURLOrNull()!!.apply {
+            assertNull(protocol)
+        }
+        "://binom/".toURLOrNull()!!.apply {
+            assertEquals("", protocol)
+        }
+    }
+
+    @Test
+    fun hostTest() {
+        "https://binom/".toURLOrNull()!!.apply {
+            assertEquals("binom", host)
+        }
+        "https://binom".toURLOrNull()!!.apply {
+            assertEquals("binom", host)
+        }
+
+        "//binom/".toURLOrNull()!!.apply {
+            assertEquals("binom", host)
+        }
+
+        "//a:b@binom/olo".toURLOrNull()!!.apply {
+            assertEquals("binom", host)
+        }
+    }
+
+    @Test
+    fun portTest() {
+        "https://binom/olo".toURLOrNull()!!.apply {
+            assertNull(port)
+        }
+        "https://binom:80/olo".toURLOrNull()!!.apply {
+            assertEquals(80, port)
+        }
+        "https://binom?".toURLOrNull()!!.apply {
+            assertNull(port)
+        }
+
+        "https://binom:80?q".toURLOrNull()!!.apply {
+            assertEquals(80, port)
+        }
+
+        "https://binom:80#q".toURLOrNull()!!.apply {
+            assertEquals(80, port)
+        }
+    }
+
+    @Test
+    fun uriTest() {
+        "https://binom/olo".toURLOrNull()!!.apply {
+            assertEquals("/olo", uri)
+        }
+
+        "https://binom/olo?q".toURLOrNull()!!.apply {
+            assertEquals("/olo", uri)
+        }
+        "https://binom/olo?q#v".toURLOrNull()!!.apply {
+            assertEquals("/olo", uri)
+        }
+        "https://binom/olo#v".toURLOrNull()!!.apply {
+            assertEquals("/olo", uri)
+        }
+
+        "https://binom".toURLOrNull()!!.apply {
+            assertEquals("", uri)
+        }
+        "https://binom:80".toURLOrNull()!!.apply {
+            assertEquals("", uri)
+        }
+        "https://binom:80?q".toURLOrNull()!!.apply {
+            assertEquals("", uri)
+        }
+        "https://binom:80#q".toURLOrNull()!!.apply {
+            assertEquals("", uri)
+        }
+    }
+
+    @Test
+    fun common() {
+        "https://user:password@binom:53/test/addr?q=1#getData".toURLOrNull()!!.apply {
+            assertEquals("https", protocol)
+            assertEquals("user", user)
+        }
+
+        "//user:password@binom:53/test/addr?q=1#getData".toURLOrNull()!!.apply {
+            assertNull(protocol)
+        }
+    }
+
+    @Test
+    fun `protoAddresPort`() {
         val url = URL("http://127.0.0.1:4646")
         assertEquals("http", url.protocol)
         assertEquals("", url.uri)
@@ -15,7 +130,7 @@ class TestURL {
     }
 
     @Test
-    fun `proto addres port uri`() {
+    fun `protoAddresPortUri`() {
         val url = URL("http://127.0.0.1:4646/")
         assertEquals("http", url.protocol)
         assertEquals("/", url.uri)
@@ -23,7 +138,7 @@ class TestURL {
     }
 
     @Test
-    fun `proto addres`() {
+    fun `protoAddres`() {
         val url = URL("http://127.0.0.1")
         assertEquals("http", url.protocol)
         assertEquals("", url.uri)
@@ -31,7 +146,7 @@ class TestURL {
     }
 
     @Test
-    fun `proto addres uri`() {
+    fun `protoAddresUri`() {
         val url = URL("http://127.0.0.1/")
         assertEquals("http", url.protocol)
         assertEquals("/", url.uri)
@@ -39,17 +154,17 @@ class TestURL {
     }
 
     @Test
-    fun `with uri`() {
+    fun `withUri`() {
         var url = URL("http://127.0.0.1:4646")
         assertEquals("127.0.0.1", url.host)
         assertEquals(4646, url.port)
-        url = url.newURI("${url.uri}/var")
+        url = url.copy(uri = "${url.uri}/var")
 
         assertEquals("/var", url.uri)
     }
 
     @Test
-    fun `without proto`() {
+    fun `withoutProto`() {
         val url = URL("//127.0.0.1:4646")
         assertNull(url.protocol)
         assertEquals("127.0.0.1", url.host)
@@ -58,10 +173,15 @@ class TestURL {
     }
 
     @Test
-    fun test() {
-        val url = URL("http://127.0.0.1/addr/pp:dd")
-        assertEquals("127.0.0.1", url.host)
-        assertNull(url.port)
-        assertEquals("/addr/pp:dd", url.uri)
+    fun toURLOrNullTest() {
+        assertNull("".toURLOrNull())
+        assertNotNull("//".toURLOrNull()).apply {
+            assertEquals("", host)
+            assertNull(port)
+            assertNull(protocol)
+            assertNull(query)
+            assertNull(hash)
+        }
+        assertNull("ht?p://".toURLOrNull())
     }
 }
