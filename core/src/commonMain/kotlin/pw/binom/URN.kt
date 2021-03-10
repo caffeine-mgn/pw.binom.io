b6f0b33b4bef013b4ec794faa91b2ea553aa0a51
caffeine-mgn <caffeine.mgn@gmail.com>
2021-03-10 12:04:14 +0300

    Http Server Reimplement

--- core/src/commonMain/kotlin/pw/binom/URN.kt
+++ core/src/commonMain/kotlin/pw/binom/URN.kt
@@ -1,11 +1,24 @@
 package pw.binom
 
+/**
+ * Contains URN String
+ * @see <a href="https://en.wikipedia.org/wiki/Uniform_Resource_Name">URN</a>
+ */
 inline class URN internal constructor(val raw: String) {
+    /**
+     * Returns true if this urn match for [mask]. Path Variables is support.
+     * Example: `"/users/100500/info_data".toURN.isMatch("/users/{id}/info_*")` returns true
+     */
     fun isMatch(mask: String) = raw.isPathMatch(mask, 0, 0)
 
-    fun getVariable(pathName: String, mask: String): String? {
+    /**
+     * Returns variable from this urn. Will find [name] inside this, using [mask].
+     * Example: if urn=`/users/100500/info_data`, then getVariable("id", "/users/{id}/info_*") will return `100500`
+     * @param mask mask for getting veriable
+     */
+    fun getVariable(name: String, mask: String): String? {
         var str: String? = null
-        raw.findPathVariable(mask, pathName, 0, 0) {
+        raw.findPathVariable(mask, name, 0, 0) {
             str = it
         }
         return str
