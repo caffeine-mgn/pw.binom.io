70abc31654b29a461a00915ec3f1f4a366c1f7fa
caffeine-mgn <caffeine.mgn@gmail.com>
2021-03-14 01:47:34 +0300

    fix

--- core/src/commonMain/kotlin/pw/binom/io/BufferedOutputAppendable.kt
+++ core/src/commonMain/kotlin/pw/binom/io/BufferedOutputAppendable.kt
@@ -4,13 +4,65 @@ import pw.binom.*
 import pw.binom.charset.Charset
 import pw.binom.charset.CharsetTransformResult
 import pw.binom.charset.Charsets
+import pw.binom.pool.ObjectPool
 
-class BufferedOutputAppendable(
+class BufferedOutputAppendable private constructor (
     charset: Charset,
     val output: Output,
-    private val pool: ByteBufferPool,
-    charBufferSize: Int = DEFAULT_BUFFER_SIZE / 2
+    private val pool: ObjectPool<ByteBuffer>?,
+    charBufferSize: Int,
+    val closeParent: Boolean,
+    private val buffer: ByteBuffer,
+    private var closeBuffer: Boolean,
 ) : Appendable, Flushable, Closeable {
+
+    constructor(
+        charset: Charset = Charsets.UTF8,
+        output: Output,
+        pool: ObjectPool<ByteBuffer>,
+        charBufferSize: Int = DEFAULT_BUFFER_SIZE / 2,
+        closeParent: Boolean = true,
+    ) : this(
+        charset = charset,
+        output = output,
+        pool = pool,
+        charBufferSize = charBufferSize,
+        closeParent = closeParent,
+        buffer = pool.borrow(),
+        closeBuffer = false,
+    )
+
+    constructor(
+        charset: Charset = Charsets.UTF8,
+        output: Output,
+        bufferSize: Int = DEFAULT_BUFFER_SIZE,
+        charBufferSize: Int = bufferSize / 2,
+        closeParent: Boolean = true,
+    ) : this(
+        charset = charset,
+        output = output,
+        pool = null,
+        charBufferSize = charBufferSize,
+        closeParent = closeParent,
+        buffer = ByteBuffer.alloc(bufferSize),
+        closeBuffer = true,
+    )
+
+    constructor(
+        charset: Charset = Charsets.UTF8,
+        output: Output,
+        buffer: ByteBuffer,
+        charBufferSize: Int = buffer.capacity / 2,
+        closeParent: Boolean = true,
+    ) : this(
+        charset = charset,
+        output = output,
+        pool = null,
+        charBufferSize = charBufferSize,
+        closeParent = closeParent,
+        buffer = buffer,
+        closeBuffer = false,
+    )
 
     val charBuffer = CharBuffer.alloc(charBufferSize)
     val encoder = charset.newEncoder()
@@ -80,46 +132,76 @@ class BufferedOutputAppendable(
         if (charBuffer.remaining == charBuffer.capacity) {
             return
         }
-        val buffer = pool.borrow()
-        try {
-            charBuffer.flip()
-            while (true) {
+        charBuffer.flip()
+        while (true) {
+            buffer.clear()
+            val r = encoder.encode(charBuffer, buffer)
+            buffer.flip()
+            if (buffer.remaining != buffer.capacity) {
+                output.write(buffer)
                 buffer.clear()
-                val oldr = charBuffer.remaining
-                val r = encoder.encode(charBuffer, buffer)
-                if (buffer.position > 0) {
-                    buffer.flip()
-                    output.write(buffer)
-                    buffer.clear()
-                }
-                if (r == CharsetTransformResult.INPUT_OVER) {
-                    println("charBuffer.remaining: ${charBuffer.remaining}  ${charBuffer[14]}")
-                    TODO()
-                }
-                if (r == CharsetTransformResult.SUCCESS || charBuffer.remaining == 0) {
-                    break
-                }
             }
-            charBuffer.clear()
-        } finally {
-            pool.recycle(buffer)
+            if (r == CharsetTransformResult.SUCCESS || charBuffer.remaining == 0) {
+                break
+            }
         }
+        charBuffer.clear()
     }
 
     override fun close() {
         checkClosed()
         flush()
-        encoder.close()
+        try {
+            encoder.close()
+            if (closeBuffer) {
+                buffer.close()
+            } else {
+                pool?.recycle(buffer)
+            }
+            if (closeParent) {
+                output.close()
+            }
+        } finally {
+            closed = true
+        }
     }
 }
 
 fun Output.bufferedWriter(
-    pool: ByteBufferPool,
+    pool: ObjectPool<ByteBuffer>,
     charset: Charset = Charsets.UTF8,
-    charBufferSize: Int = DEFAULT_BUFFER_SIZE / 2,
+    charBufferSize: Int = 512,
+    closeParent: Boolean = true
 ) = BufferedOutputAppendable(
     charset = charset,
     output = this,
+    pool = pool,
     charBufferSize = charBufferSize,
-    pool = pool
+    closeParent = closeParent
+)
+
+fun Output.bufferedWriter(
+    bufferSize: Int = DEFAULT_BUFFER_SIZE,
+    charset: Charset = Charsets.UTF8,
+    charBufferSize: Int = bufferSize / 2,
+    closeParent: Boolean = true
+) = BufferedOutputAppendable(
+    charset = charset,
+    output = this,
+    bufferSize = bufferSize,
+    charBufferSize = charBufferSize,
+    closeParent = closeParent
+)
+
+fun Output.bufferedWriter(
+    buffer: ByteBuffer,
+    charset: Charset = Charsets.UTF8,
+    charBufferSize: Int = buffer.capacity / 2,
+    closeParent: Boolean = true
+) = BufferedOutputAppendable(
+    charset = charset,
+    output = this,
+    buffer = buffer,
+    charBufferSize = charBufferSize,
+    closeParent = closeParent
 )
