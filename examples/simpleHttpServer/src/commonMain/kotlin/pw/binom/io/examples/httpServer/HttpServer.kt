801a7932a29458121efdfa5f2caa8d4f7a64b080
caffeine-mgn <caffeine.mgn@gmail.com>
2021-05-04 01:04:46 +0300

    fix

--- examples/simpleHttpServer/src/commonMain/kotlin/pw/binom/io/examples/httpServer/HttpServer.kt
+++ examples/simpleHttpServer/src/commonMain/kotlin/pw/binom/io/examples/httpServer/HttpServer.kt
@@ -4,36 +4,36 @@ import pw.binom.*
 import pw.binom.io.file.AccessType
 import pw.binom.io.file.File
 import pw.binom.io.file.channel
+import pw.binom.io.file.relative
 import pw.binom.io.http.Headers
-import pw.binom.io.httpServer.Handler3Deprecated
-import pw.binom.io.httpServer.HttpRequestDeprecated
-import pw.binom.io.httpServer.HttpResponseDeprecated
-import pw.binom.io.httpServer.HttpServer3
+import pw.binom.io.httpServer.*
 import pw.binom.io.use
 import pw.binom.network.NetworkAddress
 import pw.binom.network.NetworkDispatcher
 
 fun main() {
     println("Environment.workDirectory: ${Environment.workDirectory}")
-    val byteDataPool = ByteBufferPool(10)
     val nioManager = NetworkDispatcher()
-    val server = HttpServer3(nioManager, object : Handler3Deprecated {
-        override suspend fun request(req: HttpRequestDeprecated, resp: HttpResponseDeprecated) {
-            val file = File(File(Environment.workDirectory), req.uri)
+    val server = HttpServer(nioManager, object : Handler {
+        override suspend fun request(req: HttpRequest) {
+            val file = File(Environment.workDirectory).relative(req.request)
             if (!file.isFile) {
-                resp.status = 404
-                resp.complete()
+                req.response { it.status = 404 }
             } else {
-                resp.status = 200
-                resp.addHeader(Headers.CONTENT_TYPE, "application/octet-stream")
-                resp.addHeader(Headers.CONTENT_LENGTH, file.size.toString())
-                file.channel(AccessType.READ).use { channel ->
-                    channel.copyTo(resp.complete(), byteDataPool)
+                req.response { resp ->
+                    resp.headers.contentType = "application/octet-stream"
+                    resp.headers.contentLength = file.size.toULong()
+                    resp.status = 200
+                    file.channel(AccessType.READ).use { channel ->
+                        resp.writeBinary().use { out ->
+                            channel.copyTo(out)
+                        }
+                    }
                 }
             }
         }
-    }, outputBufferSize = 1024 * 1024 * 3)
-    server.bindHTTP(NetworkAddress.Immutable(port = 8080))
+    })
+    server.bindHttp(NetworkAddress.Immutable(port = 8080))
     while (true) {
         nioManager.select()
     }
