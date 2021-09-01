c65eb9adc4012a0c6c1f5e997a8d1ae6ed6ab47c
caffeine-mgn <caffeine.mgn@gmail.com>
2021-09-02 00:11:44 +0300

    Kotlin Version upgrade

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,12 +7,12 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
     }
 }
 
 plugins{
-        kotlin("jvm") version "1.5.20"
+        kotlin("jvm") version "1.5.30"
 }
 
 repositories {
@@ -22,9 +22,9 @@ repositories {
 }
 
 dependencies {
-    api("org.jetbrains.kotlin:kotlin-stdlib:1.5.20")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
-    api("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.5.20")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+    api("org.jetbrains.kotlin:kotlin-stdlib:1.5.30")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
+    api("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.5.30")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
     api("org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
 }
---
+++ buildSrc/src/main/kotlin/pw/binom/Utils.kt
@@ -0,0 +1,19 @@
+package pw.binom
+
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.TaskContainer
+
+fun TaskContainer.eachKotlinTest(func: (Task) -> Unit) {
+    this.mapNotNull { it as? org.jetbrains.kotlin.gradle.tasks.KotlinTest }
+        .forEach(func)
+    this.mapNotNull { it as? org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmTest }
+        .forEach(func)
+}
+
+fun TaskContainer.eachKotlinCompile(func: (Task) -> Unit) {
+    this.mapNotNull { it as? org.jetbrains.kotlin.gradle.tasks.AbstractKotlinCompile<*> }
+        .forEach(func)
+    this.mapNotNull { it as? org.jetbrains.kotlin.gradle.tasks.AbstractKotlinNativeCompile<*, *> }
+        .forEach(func)
+}
