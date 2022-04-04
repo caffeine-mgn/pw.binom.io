789cd23122559f55e51e5367f34e61e642437ac0
subochev <caffeine.mgn@gmail.com>
2022-04-04 23:03:40 +0300

    fix

--- core/src/commonMain/kotlin/pw/binom/net/URL.kt
+++ core/src/commonMain/kotlin/pw/binom/net/URL.kt
@@ -219,15 +219,15 @@ value class URL internal constructor(val fullPath: String) {
      * @param encode flag for automatic encode appending [path]
      * @return URI with appended [path]
      */
-    fun addPathString(path: String, direction: Boolean = true, encode: Boolean = false) = run {
+    fun appendPath(path: String, direction: Boolean = true, encode: Boolean = false) = run {
         val result = this.path.append(path = if (encode) UTF8.urlEncode(path) else path, direction = direction)
         copy(
             path = result
         )
     }
 
-    fun addPath(path: Path) =
-        addPathString(path = path.toString(), direction = true, encode = true)
+    fun appendPath(path: Path) =
+        appendPath(path = path.toString(), direction = true, encode = true)
 
     fun appendQuery(key: String)=copy(query = query?.append(key = key, value = null) ?: Query.new(key = key, value = null))
 
