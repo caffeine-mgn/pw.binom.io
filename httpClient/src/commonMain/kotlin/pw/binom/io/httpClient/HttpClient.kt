71fad3680ba105d97712c04660070a551649c76d
subochev <caffeine.mgn@gmail.com>
2023-06-14 01:35:37 +0300

    fix

--- httpClient/src/commonMain/kotlin/pw/binom/io/httpClient/HttpClient.kt
+++ httpClient/src/commonMain/kotlin/pw/binom/io/httpClient/HttpClient.kt
@@ -2,18 +2,9 @@ package pw.binom.io.httpClient
 
 import kotlinx.coroutines.Dispatchers
 import pw.binom.DEFAULT_BUFFER_SIZE
-import pw.binom.crypto.Sha1MessageDigest
-import pw.binom.io.AsyncChannel
 import pw.binom.io.Closeable
-import pw.binom.io.IOException
 import pw.binom.io.http.HTTPMethod
-import pw.binom.io.http.HashHeaders2
 import pw.binom.io.http.Headers
-import pw.binom.io.http.emptyHeaders
-import pw.binom.io.http.websocket.HandshakeSecret
-import pw.binom.io.http.websocket.InvalidSecurityKeyException
-import pw.binom.io.http.websocket.WebSocketConnection
-import pw.binom.io.http.websocket.WebSocketConnectionImpl2
 import pw.binom.io.httpClient.protocol.ConnectFactory2
 import pw.binom.io.httpClient.protocol.ProtocolSelector
 import pw.binom.io.httpClient.protocol.ProtocolSelectorBySchema
@@ -24,77 +15,7 @@ import pw.binom.network.Network
 import pw.binom.network.NetworkManager
 import pw.binom.url.URL
 
-class WebSocketRequest(val url: URL, val method: String, val masking: Boolean, val client: HttpClient) {
-    val headers = HashHeaders2()
-    private var started = false
-
-    suspend fun start(): WebSocketConnection {
-        check(!started) { "Connection already started" }
-        started = true
-        val requestKey = HandshakeSecret.generateRequestKey()
-        val responseKey = HandshakeSecret.generateResponse(Sha1MessageDigest(), requestKey)
-        headers[Headers.SEC_WEBSOCKET_KEY] = requestKey
-
-        val request = client.startConnect(method = method, uri = url, headers = headers, keepAlive = false)
-        val resp = request.flush()
-        if (resp.responseCode != 101) {
-            throw IOException("Invalid Response code: ${resp.responseCode}")
-        }
-        val respKey = resp.headers.getSingleOrNull(Headers.SEC_WEBSOCKET_ACCEPT)
-            ?: throw IOException("Invalid Server Response. Missing header \"${Headers.SEC_WEBSOCKET_ACCEPT}\"")
-        if (respKey != responseKey) {
-            throw InvalidSecurityKeyException()
-        }
-
-        val connection = WebSocketConnectionImpl2 { }
-        connection.reset(input = request.input, output = request.output, masking = masking)
-        return connection
-    }
-}
-
-fun HttpClient.connectWebSocket(
-    uri: URL,
-    headers: Headers = emptyHeaders(),
-    masking: Boolean = true,
-    method: String = "GET",
-): WebSocketRequest {
-    val wsRequest = WebSocketRequest(
-        client = this,
-        url = uri,
-        method = method,
-        masking = masking,
-    )
-    wsRequest.headers[Headers.HOST] = uri.host + (uri.port?.let { ":$it" } ?: "")
-    wsRequest.headers[Headers.CONNECTION] = Headers.UPGRADE
-    wsRequest.headers[Headers.UPGRADE] = Headers.WEBSOCKET
-    wsRequest.headers[Headers.SEC_WEBSOCKET_VERSION] = "13"
-    wsRequest.headers.add(headers)
-    return wsRequest
-}
-
-suspend fun HttpClient.connectTcp(
-    uri: URL,
-    headers: Headers,
-    method: String = "GET",
-): AsyncChannel {
-    val h = HashHeaders2(headers)
-    h[Headers.CONNECTION] = Headers.UPGRADE
-    h[Headers.UPGRADE] = Headers.TCP
-    val request = startConnect(method = method, uri = uri, headers = headers, keepAlive = false)
-    val resp = request.flush()
-    if (resp.responseCode != 101) {
-        throw IOException("Invalid Response code: ${resp.responseCode}")
-    }
-    HttpMetrics.defaultHttpRequestCountMetric.dec()
-    return AsyncChannel.create(
-        input = request.input,
-        output = request.output,
-    )
-//    return request.channel.channel
-}
-
 interface HttpClient : Closeable {
-//    val networkDispatcher: NetworkDispatcher
 
     suspend fun startConnect(
         method: String,
@@ -142,7 +63,7 @@ fun HttpClient.Companion.create(
         "http",
         "ws",
     )
-    var protocolSelector = ProtocolSelectorBySchema()
+    val protocolSelector = ProtocolSelectorBySchema()
     protocolSelector.set(
         HttpSSLConnectFactory2(networkManager = networkDispatcher, protocolSelector = baseProtocolSelector),
         "https",
---
+++ httpClient/src/commonMain/kotlin/pw/binom/io/httpClient/TcpRequest.kt
@@ -0,0 +1,43 @@
+package pw.binom.io.httpClient
+
+import pw.binom.io.AsyncChannel
+import pw.binom.io.IOException
+import pw.binom.io.http.HashHeaders2
+import pw.binom.io.http.Headers
+import pw.binom.url.URL
+
+class TcpRequest(val url: URL, val method: String, val client: HttpClient) {
+    val headers = HashHeaders2()
+    private var started = false
+    suspend fun start(): AsyncChannel {
+        check(!started) { "Connection already started" }
+        started = true
+        val request = client.startConnect(method = method, uri = url, headers = headers, keepAlive = false)
+        val resp = request.flush()
+        if (resp.responseCode != 101) {
+            throw IOException("Invalid Response code: ${resp.responseCode}")
+        }
+        HttpMetrics.defaultHttpRequestCountMetric.dec()
+        return AsyncChannel.create(
+            input = request.input,
+            output = request.output,
+        )
+    }
+}
+
+fun HttpClient.connectTcp(
+    uri: URL,
+    headers: Headers,
+    method: String = "GET",
+): TcpRequest {
+    val req = TcpRequest(
+        url = uri,
+        method = method,
+        client = this,
+    )
+    req.headers[Headers.CONNECTION] = Headers.UPGRADE
+    req.headers[Headers.UPGRADE] = Headers.TCP
+    req.headers[Headers.HOST] = uri.host + (uri.port?.let { ":$it" } ?: "")
+    req.headers.add(headers)
+    return req
+}
