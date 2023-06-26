6f85453a695c3d426cfc2ad9a47dded36c376981
subochev <caffeine.mgn@gmail.com>
2023-06-26 23:58:25 +0300

    fix

--- db/redis/src/commonMain/kotlin/pw/binom/db/radis/RadisConnection.kt
+++ db/redis/src/commonMain/kotlin/pw/binom/db/radis/RadisConnection.kt
@@ -3,6 +3,7 @@ package pw.binom.db.radis
 import kotlinx.coroutines.Dispatchers
 import pw.binom.io.AsyncCloseable
 import pw.binom.io.socket.InetNetworkAddress
+import pw.binom.io.socket.NetworkAddress
 import pw.binom.network.Network
 import pw.binom.network.NetworkManager
 import pw.binom.network.tcpConnect
@@ -16,13 +17,13 @@ interface RadisConnection : AsyncCloseable {
 
     companion object {
         suspend fun connect(
-            address: InetNetworkAddress,
+            address: NetworkAddress,
             manager: NetworkManager = Dispatchers.Network,
             login: String? = null,
             password: String? = null,
         ): RadisConnectionImpl {
             val con = RadisConnectionImpl(
-                connection = manager.tcpConnect(address),
+                connection = manager.tcpConnect(address.resolve()),
                 bufferSize = 30,
             )
             try {
