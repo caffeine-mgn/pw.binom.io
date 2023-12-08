dbd5735d6cfc553a1923ef3fc7a6147b9e6e0f21
subochev <caffeine.mgn@gmail.com>
2023-12-08 12:04:47 +0300

    Добавил поддержку сетевых интерфейсов

--- flux/src/commonMain/kotlin/pw/binom/flux/CachingHttpServerExchange.kt
+++ flux/src/commonMain/kotlin/pw/binom/flux/CachingHttpServerExchange.kt
@@ -9,91 +9,94 @@ import pw.binom.io.http.HashHeaders2
 import pw.binom.io.http.Headers
 import pw.binom.io.http.MutableHeaders
 import pw.binom.io.httpServer.HttpServerExchange
+import pw.binom.io.socket.InetNetworkAddress
 import pw.binom.url.Path
 import pw.binom.url.URI
 
 class CachingHttpServerExchange(val source: HttpServerExchange) : HttpServerExchange {
-    override val requestURI: URI
-        get() = source.requestURI
-    override val input: AsyncInput
-        get() {
-            val inputStream = inputStream
-            if (inputStream != null) {
-                return inputStream
-            }
-            val i = source.input
-            inputAlreadyReading = true
-            return i
-        }
-    override val output: AsyncOutput
-        get() {
-            check(headersSent) { "Output Stream access only after header send" }
-            return outputStream ?: throw IllegalStateException("Output stream not ready")
-        }
-    override val requestHeaders: Headers
-        get() = source.requestHeaders
-    override val requestMethod: String
-        get() = source.requestMethod
-    override val responseStarted: Boolean
-        get() = headersSent
+  override val requestURI: URI
+    get() = source.requestURI
+  override val input: AsyncInput
+    get() {
+      val inputStream = inputStream
+      if (inputStream != null) {
+        return inputStream
+      }
+      val i = source.input
+      inputAlreadyReading = true
+      return i
+    }
+  override val output: AsyncOutput
+    get() {
+      check(headersSent) { "Output Stream access only after header send" }
+      return outputStream ?: throw IllegalStateException("Output stream not ready")
+    }
+  override val requestHeaders: Headers
+    get() = source.requestHeaders
+  override val requestMethod: String
+    get() = source.requestMethod
+  override val responseStarted: Boolean
+    get() = headersSent
   override val requestContext: Path
     get() = source.requestContext
+  override val address: InetNetworkAddress
+    get() = source.address
 
   var bufferedOutput: ByteArrayOutput? = null
-        private set
-    private var outputStream: AsyncOutput? = null
+    private set
+  private var outputStream: AsyncOutput? = null
 
-    private var headersSent = false
+  private var headersSent = false
 
-    var responseStatus = 0
-    var responseHeaders: MutableHeaders = HashHeaders2()
-    var inputData: ByteArray? = null
-    var inputStream: AsyncInput? = null
-    private var inputCached = false
-    private var inputAlreadyReading = false
-    suspend fun readInput(bufferSize: Int = DEFAULT_BUFFER_SIZE) {
-        ByteBuffer(bufferSize).use { buffer ->
-            readInput(buffer)
-        }
+  var responseStatus = 0
+  var responseHeaders: MutableHeaders = HashHeaders2()
+  var inputData: ByteArray? = null
+  var inputStream: AsyncInput? = null
+  private var inputCached = false
+  private var inputAlreadyReading = false
+  suspend fun readInput(bufferSize: Int = DEFAULT_BUFFER_SIZE) {
+    ByteBuffer(bufferSize).use { buffer ->
+      readInput(buffer)
     }
+  }
 
-    suspend fun readInput(buffer: ByteBuffer) {
-        check(!inputAlreadyReading) { "Input already reading" }
-        check(!inputCached) { "Input already cached" }
-        ByteArrayOutput().use { buf ->
-            source.input.copyTo(buf, buffer = buffer)
-            val inputData = buf.locked {
-                it.toByteArray()
-            }
-            this.inputData = inputData
-            inputStream = ByteArrayInput(inputData).asyncInput()
-            inputCached = true
-        }
+  suspend fun readInput(buffer: ByteBuffer) {
+    check(!inputAlreadyReading) { "Input already reading" }
+    check(!inputCached) { "Input already cached" }
+    ByteArrayOutput().use { buf ->
+      source.input.copyTo(buf, buffer = buffer)
+      val inputData = buf.locked {
+        it.toByteArray()
+      }
+      this.inputData = inputData
+      inputStream = ByteArrayInput(inputData).asyncInput()
+      inputCached = true
     }
+  }
 
-    override suspend fun startResponse(statusCode: Int, headers: Headers) {
-        check(!headersSent) { "Headers already sent" }
-        responseStatus = statusCode
-        headersSent = true
-        responseHeaders.clear()
-        responseHeaders.add(headers)
-        val bufferedOutput = ByteArrayOutput()
-        this.bufferedOutput = bufferedOutput
-        outputStream = bufferedOutput.asyncOutput(callClose = false)
+  override suspend fun startResponse(statusCode: Int, headers: Headers) {
+    check(!headersSent) { "Headers already sent" }
+    responseStatus = statusCode
+    headersSent = true
+    responseHeaders.clear()
+    responseHeaders.add(headers)
+    val bufferedOutput = ByteArrayOutput()
+    this.bufferedOutput = bufferedOutput
+    outputStream = bufferedOutput.asyncOutput(callClose = false)
+  }
+
+  private var flushed = false
+
+  suspend fun flush() {
+    if (flushed) {
+      return
     }
-
-    private var flushed = false
-
-    suspend fun flush() {
-        if (flushed) {
-            return
-        }
-        flushed = true
-        source.startResponse(responseStatus, responseHeaders)
-        bufferedOutput?.use { buffer ->
-            buffer.locked { data ->
-                source.output.writeFully(data)
-            }
-        }
+    flushed = true
+    source.startResponse(responseStatus, responseHeaders)
+    bufferedOutput?.use { buffer ->
+      buffer.locked { data ->
+        source.output.writeFully(data)
+      }
     }
+  }
 }
--- socket/src/jvmLikeMain/kotlin/pw/binom/io/socket/NetworkInterface.kt
+++ socket/src/jvmLikeMain/kotlin/pw/binom/io/socket/NetworkInterface.kt
@@ -1,7 +1,5 @@
 package pw.binom.io.socket
 
-import pw.binom.dump
-import pw.binom.toByteArray
 import java.net.NetworkInterface as JNetworkInterface
 
 private fun ip4ToString(data: ByteArray): String {
@@ -34,45 +32,6 @@ private fun ipToString(data: ByteArray) =
     16 -> ip6ToString(data)
     else -> TODO()
   }
-
-private fun ipv4Mask(prefixLength: Int): String {
-  val m = (UInt.MAX_VALUE) shl (32 - prefixLength)
-  return ip4ToString(m.toInt().toByteArray())
-}
-
-private fun ipv6Mask(prefixLength: Int): String {
-  val ip = ByteArray(16)
-  return when {
-    prefixLength == Long.SIZE_BITS -> {
-      repeat(Long.SIZE_BYTES) {
-        ip[it] = UByte.MAX_VALUE.toByte()
-      }
-      ip6ToString(ip)
-    }
-
-    prefixLength == Long.SIZE_BITS * 2 -> {
-      repeat(Long.SIZE_BYTES * 2) {
-        ip[it] = UByte.MAX_VALUE.toByte()
-      }
-      ip6ToString(ip)
-    }
-
-    prefixLength < Long.SIZE_BITS -> {
-      (ULong.MAX_VALUE shl (Long.SIZE_BITS - prefixLength)).toLong().dump(ip)
-      ip6ToString(ip)
-    }
-
-    prefixLength < Long.SIZE_BITS * 2 -> {
-      repeat(Long.SIZE_BYTES) {
-        ip[it] = UByte.MAX_VALUE.toByte()
-      }
-      (ULong.MAX_VALUE shl (Long.SIZE_BITS - prefixLength)).toLong().dump(ip, destOffset = Long.SIZE_BYTES)
-      ip6ToString(ip)
-    }
-
-    else -> TODO("prefixLength=$prefixLength")
-  }
-}
 
 internal actual fun getAvailableNetworkInterfaces(): List<NetworkInterface> {
   val out = ArrayList<NetworkInterface>()
