e0bfeb0139ca5d42c58c70078f0ad69ab0becf1e
Caffeine <caffeine.mgn@gmail.com>
2025-01-01 17:06:48 +0300

    adding macos support

--- concurrency/src/appleMain/kotlin/pw/binom/concurrency/ThreadRef.kt
+++ concurrency/src/appleMain/kotlin/pw/binom/concurrency/ThreadRef.kt
@@ -1,6 +1,8 @@
 package pw.binom.concurrency
 
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.posix.*
 
+@OptIn(ExperimentalForeignApi::class)
 internal actual val currentThreadId: Long
     get() = pthread_self()!!.rawValue.toLong()
--- pool/src/appleMain/kotlin/pw/binom/pool/ThreadUtils.kt
+++ pool/src/appleMain/kotlin/pw/binom/pool/ThreadUtils.kt
@@ -1,7 +1,9 @@
 package pw.binom.pool
 
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.posix.*
 
+@OptIn(ExperimentalForeignApi::class)
 internal actual object ThreadUtils {
     actual val currentThreadId: Long = pthread_self()!!.rawValue.toLong()
 }
