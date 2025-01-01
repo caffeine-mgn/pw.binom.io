4397f63e3633b869a3c3137985a6c563a9e5dc0d
Caffeine <caffeine.mgn@gmail.com>
2025-01-01 19:21:22 +0300

    adding macos support

---
+++ charset/src/appleMain/kotlin/pw/binom/charset/Iconv.kt
@@ -0,0 +1,24 @@
+package pw.binom.charset
+
+import kotlinx.cinterop.*
+import platform.iconv.iconv
+import platform.iconv.iconv_close
+import platform.iconv.iconv_open
+import platform.posix.size_tVar
+
+
+@OptIn(ExperimentalForeignApi::class, UnsafeNumber::class)
+actual object Iconv {
+  actual fun open(tocode: String?, fromcode: String?): CPointer<out CPointed>? =
+    iconv_open(tocode, fromcode)
+
+  actual fun iconv1(
+    __cd: CPointer<out CPointed>?,
+    __inbuf: CValuesRef<CPointerVarOf<CPointer<ByteVarOf<Byte>>>>?,
+    __inbytesleft: CValuesRef<size_tVar>?,
+    __outbuf: CValuesRef<CPointerVarOf<CPointer<ByteVarOf<Byte>>>>?,
+    __outbytesleft: CValuesRef<size_tVar>?,
+  ): ULong = iconv(__cd, __inbuf, __inbytesleft, __outbuf, __outbytesleft).convert()
+
+  actual fun close(__cd: CPointer<out CPointed>?): Int = iconv_close(__cd)
+}
