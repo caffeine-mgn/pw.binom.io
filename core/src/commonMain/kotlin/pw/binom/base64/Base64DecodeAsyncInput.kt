750d6e658d694237874b49b18bf46cbda9acb2a3
subochev <caffeine.mgn@gmail.com>
2025-11-08 17:53:18 +0300

    fix

---
+++ core/src/commonMain/kotlin/pw/binom/base64/Base64DecodeAsyncInput.kt
@@ -0,0 +1,78 @@
+package pw.binom.base64
+
+import pw.binom.io.AsyncInput
+import pw.binom.io.AsyncReader
+import pw.binom.io.Available
+import pw.binom.io.ByteBuffer
+import pw.binom.io.DataTransferSize
+import pw.binom.io.Input
+import pw.binom.io.Reader
+import kotlin.experimental.or
+
+class Base64DecodeAsyncInput(val reader: AsyncReader) : AsyncInput {
+  private var counter = 0
+  private var value = 0.toByte()
+
+  private var buffer = CharArray(32)
+  private var cursor = buffer.size
+  private var len = buffer.size
+
+  private suspend fun getChar(): Char? {
+    if (cursor >= len) {
+      len = reader.read(buffer).length
+      if (len == 0)
+        return null
+      cursor = 0
+    }
+    return buffer[cursor++]
+  }
+
+  override val available: Available
+    get() = Available.UNKNOWN
+
+  override suspend fun read(dest: ByteBuffer): DataTransferSize {
+    val length = dest.remaining
+
+    while (dest.remaining > 0) {
+      val c = getChar() ?: break
+      if (c == '=') {
+        counter++
+        continue
+      }
+      val value = charFromBase64(c)
+      when (counter) {
+        0 -> {
+          this.value = this.value or (value shl 2)
+        }
+
+        1 -> {
+          val write = this.value or (value shr 4)
+          dest.put(write)
+          this.value = value shl 4
+        }
+
+        2 -> {
+          val write = this.value or (value shr 2)
+          dest.put(write)
+          this.value = value shl 6
+        }
+
+        3 -> {
+          val write = this.value or (value)
+          dest.put(write)
+          this.value = 0
+        }
+
+        else -> throw RuntimeException()
+      }
+      counter++
+      if (counter == 4)
+        counter = 0
+    }
+    return DataTransferSize.ofSize(length - dest.remaining)
+  }
+
+  override suspend fun asyncClose() {
+    // Do nothing
+  }
+}
